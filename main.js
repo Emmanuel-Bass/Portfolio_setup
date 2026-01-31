4ffad4db279c017864bf056d93e7b6b91fb8a509
Emmanuel <talk2kvngcrownedstar@gmail.com>
2026-01-31 15:37:33 +0100

    Add mobile menu toggle and link functionality

---
+++ main.js
@@ -0,0 +1,17 @@
+const menuButton = document.getElementById('menu-button');
+const nav = document.querySelector('nav');
+const menuLinks = document.querySelectorAll("nav ul li a");
+
+const toggleMenu = () => {
+    nav.classList.toggle("open");
+    menuButton.classList.toggle("open");
+}
+
+menuLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        nav.classList.remove("open");
+        menuButton.classList.remove("open");
+    });
+});
+
+menuButton.addEventListener("click", toggleMenu);
